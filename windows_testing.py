be9d543b42a22a3361ac025a038d99d1b4334c4a
Madhan Aadithya <pmaadithya09@gmail.com>
2022-01-23 16:31:06 +0530

    update

--- windows_testing.py
+++ windows_testing.py
@@ -3,8 +3,3 @@ from app_controller import windows_controller
 window = windows_controller.device()
 
 # testing codes are removed after working.
-
-window.run_command('git add .')
-window.run_command('git commit -m "update"')
-window.run_command('git pull')
-window.run_command('git push origin main')
