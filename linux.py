32df2fe44f8ee838bad5b5e047857ab70bff0311
MadhanDevlpr <pmaadithya09@gmail.com>
2022-04-30 21:05:21 +0530

    linux testing's file edited

--- linux.py
+++ linux.py
@@ -1,2 +1,3 @@
 from app_controller import linux_controller
 
+device = linux_controller.device()
--- win.py
+++ win.py
@@ -1,7 +1,5 @@
 from app_controller import windows_controller
 
-window = windows_controller.device()
+device = windows_controller.device()
 
-# * testing codes are removed after working.
 
-# * coded by MadhanDevlpr
